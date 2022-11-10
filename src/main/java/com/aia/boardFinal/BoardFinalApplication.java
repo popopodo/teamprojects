0279be33e5e28a7639d269aa45efc9be165b820f
DESKTOP-TIP2TGM\AugiCho <chojeyungbit@gmail.com>
2022-11-10 12:30:26 +0900

    Initializing Repository

---
+++ src/main/java/com/aia/boardFinal/BoardFinalApplication.java
@@ -0,0 +1,13 @@
+package com.aia.boardFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aia/boardFinal/BoardFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aia.boardFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
