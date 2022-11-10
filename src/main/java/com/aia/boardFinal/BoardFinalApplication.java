afa86c983ee251ae597d51e749939c207a3682b1
DESKTOP-TIP2TGM\AugiCho <chojeyungbit@gmail.com>
2022-11-10 16:22:58 +0900

    DB 생성 및 db.properties 등록

--- src/main/java/com/aia/boardFinal/BoardFinalApplication.java
+++ src/main/java/com/aia/boardFinal/BoardFinalApplication.java
@@ -2,12 +2,11 @@ package com.aia.boardFinal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.PropertySource;
+@PropertySource("classpath:db.properties")
 @SpringBootApplication
 public class BoardFinalApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BoardFinalApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BoardFinalApplication.class, args);
+    }
 }
