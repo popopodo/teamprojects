303b6ac639355bfd5ba6d2dd91d15ca057f7f9ed
DESKTOP-TIP2TGM\AugiCho <chojeyungbit@gmail.com>
2022-11-11 09:37:49 +0900

    js 적용 시작

--- src/main/java/com/aia/boardFinal/BoardFinalApplication.java
+++ src/main/java/com/aia/boardFinal/BoardFinalApplication.java
@@ -3,6 +3,7 @@ package com.aia.boardFinal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+
 @PropertySource("classpath:db.properties")
 @SpringBootApplication
 public class BoardFinalApplication {
